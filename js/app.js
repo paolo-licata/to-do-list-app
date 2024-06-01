eb0805690d2c9d639acfbfcf4eda614cc9927e86
Paolo Licata <paolo_licata@outlook.com>
2024-06-01 17:42:38 +0200

    Simple to-do app created using jQuery
    If applied, this commit will have created a simple app. You can add tasks, cross them out when done or reorder them, based on your needs

---
+++ js/app.js
@@ -0,0 +1,40 @@
+function newItem() {
+
+    //Creating "li" items
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        $('#list').append(li);
+        $('#input').val(''); //Added to ensure that the input field is reset after li.append - makes it easier for the user.
+    }
+
+    //Crossing out a "to-do" element from the list
+    function crossOut() {
+        li.toggleClass('strike');
+    }
+
+    li.on('dblclick', function crossOut() {
+        li.toggleClass('strike');
+    });
+
+    //adding the "x" button to delete a task
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('x'));
+    li.append(crossOutButton);
+
+    crossOutButton.on('click', deleteListItem);
+    function deleteListItem() {
+        li.addClass('delete');
+    }
+
+    $('#list').sortable();
+
+}
+
+
+
+
